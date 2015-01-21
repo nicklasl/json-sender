6a94f74826b90511820a45f55602e9008114f5de
Nicklas <nicklaslundin@gmail.com>
2015-01-21 21:22:05 +0100

    updateable docs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.libs.json.JsValue
 import play.api.mvc._
-import play.api.{Configuration, Play}
+import play.api.{Logger, Configuration, Play}
 
 import scala.collection.mutable
 
@@ -11,7 +11,9 @@ object Application extends Controller {
   val memory: mutable.Map[String, JsValue] = new mutable.HashMap[String, JsValue]()
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val urls = memory.keys.toList
+    Logger.debug(s"urls=$urls")
+    Ok(views.html.index(urls))
   }
 
 
@@ -22,14 +24,17 @@ object Application extends Controller {
           val maybeUsername: Option[String] = json.\("username").asOpt[String]
           val maybePassword: Option[String] = json.\("password").asOpt[String]
           (maybeUsername, maybePassword) match {
-            case (Some(username), Some(password)) => validate(username, password)
+            case (Some(username), Some(password)) => {
+              if (valid(username, password)) Ok.withSession(Session(Map("user" -> username)))
+              else Forbidden
+            }
             case _ => Forbidden
           }
       }.getOrElse(BadRequest)
   }
 
   def getDocument(docName: String) = Action {
-    memory.get(docName).map{ json =>
+    memory.get(docName).map { json =>
       Ok(json).as("application/json")
     }.getOrElse(NotFound)
   }
@@ -37,23 +42,30 @@ object Application extends Controller {
   def addDocument(docName: String) = Action {
     implicit request =>
       if (memory.get(docName).isDefined) {
-        BadRequest("duplicate name, use put to replace.")
+        BadRequest("duplicate name, use PUT to replace.")
       } else {
         request.body.asJson.map {
           json =>
             memory.put(docName, json)
-        }
-        Ok
+            Ok
+        }.getOrElse(BadRequest)
       }
   }
 
+  def replaceDocument(docName: String) = Action {
+    implicit request =>
+      request.body.asJson.map {
+        json =>
+          memory.put(docName, json)
+          Ok
+      }.getOrElse(BadRequest)
+  }
 
-  private def validate(username: String, password: String): Result = {
+  private def valid(username: String, password: String): Boolean = {
     val logins: Seq[Configuration] = Play.current.configuration.getConfigSeq("logins").get
     val option: Option[Configuration] = logins.find(entry => entry.getString("username").getOrElse("").equalsIgnoreCase(username))
-    option.map { conf => if (conf.getString("password").exists(p => p.equalsIgnoreCase(password))) {
-      Ok.withSession(Session(Map("user"->username)))
-    } else Forbidden
-    }.getOrElse(Forbidden)
+    option.exists(conf => if (conf.getString("password").exists(p => p.equalsIgnoreCase(password))) {
+      true
+    } else false)
   }
 }
